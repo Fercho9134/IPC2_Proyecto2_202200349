ed1cdbc272d3c5e08929adbce8f41adab8497880
Fernando <fercho.asensio@gmail.com>
2023-09-17 22:21:44 -0600

    Se agregaron los avances de la semana 1
    Se planeo y comenzó con la implementación de la lógica que manejara el programa, el menú ya está finalizado. Se emplearán listas doblemente enlazadas

---
+++ Proyecto 2 IPC 2/ListaDoblementeEnlazada.py
@@ -0,0 +1,46 @@
+from Nodo import Nodo
+
+class ListaDoblementeEnlazada:
+
+    def __init__(self):
+        self.inicio = None
+        self.final = None
+        self.cantidad_elementos = 0
+
+    def agregar_al_final(self, objeto):
+        nuevo_nodo = Nodo(objeto)
+        if self.cantidad_elementos == 0:
+            self.inicio = nuevo_nodo
+            self.final = nuevo_nodo
+        else:
+            nuevo_nodo.anterior = self.final
+            self.final.siguiente = nuevo_nodo
+            self.final = nuevo_nodo
+        self.cantidad_elementos += 1
+
+    def eliminar(self, objeto):
+        
+        actual = self.inicio
+
+        while actual:
+            if actual.objeto.nombre == objeto.nombre:
+                if actual.anterior:
+                    actual.anterior.siguiente = actual.siguiente
+                else:
+                    self.inicio = actual.siguiente
+                if actual.siguiente:
+                    actual.siguiente.anterior = actual.anterior
+                else:
+                    self.final = actual.anterior
+                self.cantidad_elementos -= 1
+                return
+            actual = actual.siguiente
+
+    def imprimir(self):
+        actual = self.inicio
+        while actual:
+            print(actual.objeto.nombre, end=" <-> ")
+            actual = actual.siguiente
+
+    def contar_elementos(self):
+        return self.cantidad_elementos
---
+++ Proyecto 2 IPC 2/SistemaDrones.py
@@ -0,0 +1,18 @@
+from ListaDoblementeEnlazada import ListaDoblementeEnlazada
+from Dron import Dron
+
+class SistemaDrones:
+    def __init__(self, nombre, altura_maxima, cantidad_drones):
+        self.nombre = nombre
+        self.altura_maxima = altura_maxima
+        self.cantidad_drones = cantidad_drones
+        self.drones = ListaDoblementeEnlazada()
+    
+    def agregar_dron(self, nombre):
+        if self.drones.contar_elementos() < self.cantidad_drones:
+            self.drones.agregar_al_final(Dron(nombre))
+        else:
+            print("No se pueden agregar más drones")
+
+    def eliminar_dron(self, nombre):
+        self.drones.eliminar(Dron(nombre))
